a964434e7e833127b5604cb24b3422a31f497948
jackiehep <jackie_hep@sina.com>
2019-10-29 20:19:38 +0800

    Commit again

---
+++ testtwo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class CJackie{
+    private:
+    int id;
+    string value;
+    public:
+    CJackie(int id,string value)
+    {
+        this->id=id;this->value=value;
+    }
+    void ShowInfo()
+    {
+        cout<<id<<"..."<<value<<endl;
+    }
+};
+int main()
+{
+    CJackie jack(1000,"jackie");
+    jack.ShowInfo();
+    return 1;
+}
