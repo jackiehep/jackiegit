382ef51dd1e952b82bf4c993fb706ef0a89ff03b
jackiehep <jackie_hep@sina.com>
2020-06-28 17:10:45 +0800

    Update testone.cpp
    commit this change

--- testone.cpp
+++ testone.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class ClassDefine{
+	private:
+	int id;
+	public:
+	ClassDefine(int id)
+	{
+		this->id=id;
+	}
+	void ShowIDInfo()
+	{
+		cout<<"ID Info is "<<id<<endl;
+	}
+};
 int main()
 {
-	cout<<"hello,world"<<endl;
-	string val;
-	val="english";
-	cout<<val<<endl;
-	cout<<"english book is mine"<<endl;
-	cout<<"test"<<endl;
+	ClassDefine cd(1000);
+	cd.ShowIDInfo();
 	return 1;
 }
